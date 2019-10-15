4325e57c6066090e7cba2b133cbaa039dd87404f
daniel.mwangila <margalight@outlook.com>
2019-10-15 17:49:09 +0200

    Enables the user to register themselves

---
+++ app/src/main/java/com/example/quizme/UI/Activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizme.UI.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quizme.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
